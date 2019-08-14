9d2d84dbafd33ac6758e4ace010cc09282700082
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 15:09:18 -0400

    switch

---
+++ src/switchstatement.java
@@ -0,0 +1,26 @@
+
+public class switchstatement {
+	public static void main(String[] args) {
+		
+		int age;
+		age = 2;
+		
+		switch(age) {
+		case 1:
+			System.out.println("you can crawl");
+			break;
+		case 2:
+			System.out.println("you can walk");
+			break;
+		case 3: 
+			System.out.println("you can talk");
+			break;
+		
+		default:
+			System.out.println("you are growing");
+			break;
+		}
+		
+		
+	}
+}
