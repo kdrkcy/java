67efaa00ba33f8086d7d58429399ee174db8643c
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 19:47:04 -0400

    constructors

---
+++ src/constructor.java
@@ -0,0 +1,10 @@
+
+public class constructor {	
+	public static void main(String [] args) {
+		
+		constructor1 obj = new constructor1("kelsey");
+		constructor1 obj2 = new constructor1("halsey");
+		obj.saying();
+		obj2.saying();
+	}
+}
---
+++ src/constructor1.java
@@ -0,0 +1,14 @@
+
+public class constructor1 {
+	private String mom;
+	
+	public constructor1(String name) {
+		mom=name;
+		
+		
+	}
+	public void saying() {
+		System.out.println("your mothers' name is " +mom);
+	}
+	
+}
