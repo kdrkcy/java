75fce38d0ed0517bc030c680863e3e50af337536
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 13:57:49 -0400

    increment operators

---
+++ src/increment.java
@@ -0,0 +1,14 @@
+
+public class increment {
+		public static void main(String[] args) {
+			
+			int tuna = 5;
+			int bass =12 ;
+			++tuna;
+			tuna +=5;
+			System.out.println(tuna);
+			System.out.println(bass++);
+			System.out.println(bass);
+		}
+	
+}
