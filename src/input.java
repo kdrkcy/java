99b844a6184c9388309ccfd55c2f13f6423ee133
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 13:32:55 -0400

    input/Scanner

---
+++ src/input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class input {
+	
+	public static void main (String[] args) {
+
+		System.out.println("enter your name: ");
+		Scanner bucky = new Scanner(System.in);
+		System.out.println("your name is " +bucky.nextLine());
+		
+		
+		
+	}
+
+}
