17a71ae7bd24388bc84e7bf2d844a1a3a9022b0b
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 13:22:02 -0400

    variables

--- src/apples.java
+++ src/apples.java
@@ -2,6 +2,13 @@
 public class apples {
    public static void main(String[] args) {
 	   
-	   System.out.println("Hello world");
+	   double tuna;
+	   tuna = 7.35;
+	   String kdr = "john";
+	   int x=2;
+	   
+	   
+	   System.out.println("went to the game at " +tuna +  " with " + kdr + " and  " +x+ " more friends" );
+	   
    }
 }
