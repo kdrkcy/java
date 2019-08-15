3d7ca08d853537b0f28803831b2f0279379fbb15
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 20:02:22 -0400

    else if statement

---
+++ src/elseif.java
@@ -0,0 +1,20 @@
+
+public class elseif {
+
+		public static void main(String[] args) {
+			int age=60;
+			if (age>=60) {
+				System.out.println("you are a senior citizen");
+			}
+			else if(age>=50) {
+				System.out.println("you are at your 50s");
+			}
+			else if(age>=40) {
+				System.out.println("you are at your 40s");
+			}
+			else {
+				System.out.println("you are young");
+			}
+				
+		}
+}
