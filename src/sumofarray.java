5a9a564225708f57533de94dcb25bca61028e8cc
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-15 16:03:57 -0400

    summing elements of arrays

---
+++ src/sumofarray.java
@@ -0,0 +1,13 @@
+
+public class sumofarray {
+ public static void main(String[] args) {
+	 
+	 int bucky[] = {13,4,6,9};
+	 int sum=0;
+	 for(int counter=0;counter<bucky.length;counter++) {
+		 sum+=bucky[counter];
+	 }
+	 System.out.println("the sum of the arrays' elements: " +sum);
+	 
+ }
+}
