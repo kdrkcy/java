f8edcccdde31278f184dc53674026a4a04718a89
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-15 13:34:37 -0400

    array table

---
+++ src/array.java
@@ -0,0 +1,14 @@
+
+public class array {
+	public static void main(String[] args) {
+		
+		int bucky[][]= {{12,24,36,48,60},{24,36,48,60,72}};
+		for(int count=0;count<2;count++) {
+			for(int counter=0;counter<5;counter++) {
+		System.out.println(bucky[count][counter]);	
+			}
+			System.out.println("\n");
+		}
+	
+	}
+}
