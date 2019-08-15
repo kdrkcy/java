205b3a82042128dbd1ceb35fd5731558fd2a0286
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-14 20:08:53 -0400

    conditional operators

---
+++ src/conditional.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class conditional {
+ public static void main(String[] args) {
+	 Scanner obj = new Scanner(System.in);
+	 int age = obj.nextInt();
+	 System.out.println(age>50 ? "you are old" : "you are young");
+	 
+ }
+}
