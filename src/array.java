f37f1cda0669f37663da91038352cf7efa2e4652
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-15 15:42:42 -0400

    multidimentional array table

--- src/array.java
+++ src/array.java
@@ -5,7 +5,7 @@ public class array {
 		int bucky[][]= {{12,24,36,48,60},{24,36,48,60,72}};
 		for(int count=0;count<2;count++) {
 			for(int counter=0;counter<5;counter++) {
-		System.out.println(bucky[count][counter]);	
+		System.out.print(bucky[count][counter]+"\t");	
 			}
 			System.out.println("\n");
 		}
