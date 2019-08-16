3cd4f7268a81b57367708c9461c8386c49146531
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-15 23:15:47 -0400

    arrays in methods

---
+++ src/arraymethod.java
@@ -0,0 +1,20 @@
+
+public class arraymethod {
+	public static void main(String [] args) {
+		
+	int	bucky[]= {3,4,5,6,7};
+		
+	change(bucky);	
+	for(int y: bucky) {
+	System.out.print(y+" ");
+	}
+	
+	}
+	
+	public static void change(int x[]) {
+		for(int counter=0;counter<x.length;counter++) {
+			x[counter]+=5;
+		}
+		
+	}
+}
