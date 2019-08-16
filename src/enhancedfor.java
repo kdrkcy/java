539f42e90d0e46e429709af8d20dd4b4e1d78fc4
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-15 22:35:33 -0400

    enhanced for loop

---
+++ src/enhancedfor.java
@@ -0,0 +1,15 @@
+
+public class enhancedfor {
+	public static void main(String[] args) {
+		int bucky[]= {12,24,36,48,60};
+		int sum=0;
+		for(int num : bucky) {
+			
+			sum+=num;
+		}
+		
+		System.out.println("the total of arrays' elements :  " +sum);
+
+		
+	}
+}
