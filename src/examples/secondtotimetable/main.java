1fc0756d4404629b425bfaa0c119b3b5471c2423
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-20 14:38:20 -0400

    seconds to time

---
+++ src/examples/secondtotimetable/main.java
@@ -0,0 +1,23 @@
+package examples.secondtotimetable;
+
+
+import java.util.Scanner;
+
+public class main {
+
+public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("input seconds :   ");
+	int seconds= input.nextInt();
+	
+	int h, m ,s;
+	s=seconds%60;
+	h=seconds/60;
+	m=h%60;
+	h=h/60;
+	System.out.printf(seconds+" seconds equeal to : %d %d %d",h,m,s);
+}
+
+}
+///1;12;33
