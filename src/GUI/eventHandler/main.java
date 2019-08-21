3e8950bf33b76c7fd8de72d3aeeb7de7bc2c40af
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-21 17:11:17 -0400

    Action handler

---
+++ src/GUI/eventHandler/main.java
@@ -0,0 +1,13 @@
+package GUI.eventHandler;
+import javax.swing.JFrame;
+public class main {
+	public static void main(String [] args) {
+		
+		tuna tunaObject = new tuna();
+		tunaObject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tunaObject.setSize(175, 200);
+		tunaObject.setVisible(true);
+		
+		
+	}
+}
