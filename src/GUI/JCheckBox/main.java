48f33ba84891185c9bfa2daea114dc7163cfd72d
KDRKCY <KDRKCY@DESKTOP-TPV839I>
2019-08-22 17:14:35 -0400

    JCheckBox

---
+++ src/GUI/JCheckBox/main.java
@@ -0,0 +1,12 @@
+package GUI.JCheckBox;
+
+import javax.swing.JFrame;
+
+public class main {
+public static void main(String[] args) {
+	tuna obj = new tuna();
+	obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	obj.setSize(300, 200);
+	obj.setVisible(true);
+}
+}
